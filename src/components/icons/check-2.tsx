6a3d43fe7957b9ba1df3636b7013465da451b6df
sn0wzZz <sn0wzzz.dev@gmail.com>
2025-01-22 16:36:40 +0200

    Le fin

---
+++ src/components/icons/check-2.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Check2Icon = (
+  props: React.JSX.IntrinsicAttributes & React.SVGProps<SVGSVGElement>
+) => {
+  return (
+    <svg
+      width='20'
+      height='19'
+      viewBox='0 0 19 20'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        fill-rule='evenodd'
+        clip-rule='evenodd'
+        d='M9.55556 19.1111C14.8329 19.1111 19.1111 14.8329 19.1111 9.55556C19.1111 4.27817 14.8329 0 9.55556 0C4.27817 0 0 4.27817 0 9.55556C0 14.8329 4.27817 19.1111 9.55556 19.1111ZM13.9835 8.01127C14.4499 7.54481 14.4499 6.78853 13.9835 6.32207C13.517 5.85561 12.7607 5.85561 12.2943 6.32207L8.36111 10.2552L6.81682 8.71096C6.35036 8.2445 5.59408 8.2445 5.12762 8.71096C4.66116 9.17741 4.66116 9.93369 5.12762 10.4002L7.51651 12.789C7.98297 13.2555 8.73925 13.2555 9.20571 12.789L13.9835 8.01127Z'
+        fill='currentColor'
+      />
+    </svg>
+  )
+}
+
+export default Check2Icon
--- src/modules/index/hero.tsx
+++ src/modules/index/hero.tsx
@@ -2,11 +2,32 @@
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 import wave from '../../../public/hero-wave.svg'
-import { ChevronRight, TrendingUp } from 'lucide-react'
+import { Check, ChevronRight, TrendingUp } from 'lucide-react'
 import Container from '@/components/container'
 import { useScroll, motion, useTransform } from 'framer-motion'
 import { useRef } from 'react'
 import CountUp from 'react-countup'
+import memojie1 from '../../../public/home/memojie-1.png'
+import memojie2 from '../../../public/home/memojie-2.png'
+import memojie3 from '../../../public/home/memojie-3.png'
+import memojie4 from '../../../public/home/memojie-4.png'
+import user1 from '../../../public/home/user-1.png'
+import user2 from '../../../public/home/user-2.png'
+import user3 from '../../../public/home/user-3.png'
+import user4 from '../../../public/home/user-4.png'
+import CheckIcon from '@/components/icons/check'
+import Check2Icon from '@/components/icons/check-2'
+
+const memojies= [
+memojie4,memojie3, memojie2, memojie1
+]
+const users = [
+  { avatar: user1, progress:75 },
+  { avatar: user2, progress:33 },
+  { avatar: user3, progress:66 },
+  { avatar: user4, progress:0 },
+  
+]
 
 export default function Hero() {
   const ref = useRef(null)
@@ -29,7 +50,7 @@ export default function Hero() {
           style={{ y }}
           initial={{ opacity: 0, y: 200 }}
           animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.8, ease: 'easeOut', delay:1 }}
+          transition={{ duration: 0.8, ease: 'easeOut', delay: 1 }}
           className='w-[512px] h-[256px] relative after:content-[""] -z-0 after:absolute after:inset-0 after:rounded-t-full after:bg-gradient-to-b from-background to-primary-muted dark:from-muted dark:to-background  after:z-20'
         />
       </div>
@@ -61,12 +82,27 @@ export default function Hero() {
         <div className='max-w-[519px] w-full p-6 dark:bg-card dark:border-t bg-gradient-to-b from-background  to-background/10  flex flex-col gap-4 rounded-[32px] relative'>
           {/* Users */}
           <div className='absolute bg-background dark:bg-card dark:border-t shadow-hero dark:shadow-[0px_44px_64px_24px_hsla(240,6%,7%,0.5))] flex p-6 rounded-3xl gap-1.5 md:-top-14 top-[240px] md:-right-6    left-8 sm:left-32 md:left-auto'>
-            {Array.from({ length: 4 }).map((_, i) => (
+            {users.map((user, i) => (
               <div
                 key={i}
-                className='h-[51px] aspect-square bg-background rounded-full flex-center border-2 border-muted '
+                className='h-[51px]  aspect-square relative bg-background rounded-full flex-center'
               >
-                {i}
+                {user.progress===0&&<span className='absolute w-5 h-5  bg-white dark:text-muted top-0 -right-1 z-10 flex justify-center items-center rounded-full border border-white text-primary '> <Check2Icon className='h-3 w-3 '/></span>}
+                <motion.div
+                  initial={{ '--progress': 0 }}
+                  animate={{ '--progress': user.progress }}
+                  transition={{ duration: 1.5, delay: i * 0.2 }}
+                  style={{ '--progress': user.progress } as React.CSSProperties}
+                  className={`absolute inset-0 rounded-full p-[2px] [background:conic-gradient(from_0deg,hsl(var(--input))_calc(var(--progress)*1%),hsl(var(--muted))_0)] dark:[background:conic-gradient(from_0deg,hsl(var(--muted))_calc(var(--progress)*1%),hsl(var(--input))_0)]`}
+                >
+                  <div className='w-full h-full bg-background rounded-full flex-center'>
+                    <Image
+                      src={user.avatar}
+                      alt='user'
+                      className='rounded-full'
+                    />
+                  </div>
+                </motion.div>
               </div>
             ))}
           </div>
@@ -80,12 +116,12 @@ export default function Hero() {
               <div>
                 {/* Memojies */}
                 <div className='flex -space-x-4'>
-                  {Array.from({ length: 4 }).map((_, i) => (
+                  {memojies.map((memojie, i) => (
                     <div
                       key={i}
-                      className='h-[41px] aspect-square bg-background rounded-full flex-center border-2 border-muted '
+                      className='h-[41px] aspect-square bg-background rounded-full overflow-hidden flex-center border-2 border-muted dark:border-primary-item '
                     >
-                      {i}
+                      <Image src={memojie} alt='memojie' />
                     </div>
                   ))}
                 </div>
